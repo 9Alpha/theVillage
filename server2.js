0caf452b07a06e2183a594fb6c78650ef8c45995
Max Mitchell <mmitchell4488@yahoo.com>
2016-01-04 22:44:12 -0500

    with two servers

--- server2.js
+++ server2.js
@@ -2,26 +2,33 @@ var  express =  require('express');
 var  path =  require('path');
 var  app =  express();
 var ejs = require('ejs');
+var verver = require('./verver');
 var bodyParser = require('body-parser');  
-var http = require('http');
+
 app.use(bodyParser.json());  
 app.use(bodyParser.urlencoded({ extended: false }));
 
 var fs = require("fs");
-
-var options = {
-	host: '',
-	path: '',
-	port: 4000
-};
 //var content = fs.readFileSync("public/index.html", 'utf8');
 
 
 app.use("/public", express.static(path.join(__dirname,'public')));
 
 app.get('/', function(req, res){
-	res.render("login.ejs");
+	res.send("This page does nothing");
 });
+
+app.get('/vilData/:id', function(req, res){
+	verver.update(req.params.id);
+	res.send(verver.sendUser(req.params.id));
+});
+
+app.put('/vilData/:id', function(req, res){
+	console.log("did stuff");
+	verver.writeToFile(req.params.id);
+	res.send("sent");
+});
+
 
 app.post('/checkLogin', function(req, res){
 	var temp = req.body;
@@ -41,46 +48,5 @@ app.put('/makeLogin', function(req, res){
 	res.send("yee");
 });
 
-app.get('/options', function(req, res){
-	res.send("options page");
-});
 
-app.put('/options', function(req, res){
-	var gotten = req.body;
-	res.send(gotten);
-});
-
-app.get('/village', function(req, res){
-	res.render("villagePage.ejs");
-});
-
-app.get('/vilData/:id', function(req, res){
-	setRoute('localhost', '/vilData/'+req.params.id);
-	http.request(options, function(response) {
-		var temp = '';
-		response.on('data', function(chunk) {
-			temp+=chunk;
-		});
-		response.on('end', function() {
-		res.send(temp);
-		});
-	}).end();
-});
-
-app.put('/vilData/:id', function(req, res){
-	console.log("did stuff");
-	//verver.writeToFile(req.params.id);
-	res.send("sent");
-});
-
-
-
-app.listen(process.env.PORT || 5000);
-
-
-setRoute = function(host, path) {
-	options.host = host;
-	options.path = path;
-}
-
-
+app.listen(process.env.PORT || 4000);
