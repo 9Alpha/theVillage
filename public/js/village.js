dd97dd5d7d7ef12cec2e5003636c977aea4604ad
teacher-admin <teacher-admin@mbpr1315-01792.home>
2016-01-09 14:17:38 -0500

    with node tree

--- public/js/village.js
+++ public/js/village.js
@@ -2,16 +2,17 @@ $drawSect = $('#drawHere');
 var villageData;
 
 var Width;
+var count = 0;
 var Height;
 
 
 function setup() {
-  var myCanvas = createCanvas(1000, 1000);
-  myCanvas.parent('drawHere');
+	var myCanvas = createCanvas(1000, 1000);
+	myCanvas.parent('drawHere');
 
 
-  Width = width;
-  Height = height;
+	Width = width;
+	Height = height;
 }
 
 function draw() {
@@ -20,14 +21,15 @@ function draw() {
 		villageData = data;
 
 		//background(145, 35, 33);
-
- for (var i = 0; i < width; i+=20) {
-  	for (var j = 0; j < height; j+=20){
-  		if (villageData.theGrid[(j/20)*(width/20)+(i/20)]) fill(255, 50);
-  		else fill(0, 50);
-  		rect(i, j, 20, 20);
-  	}
-  }
+		if (count % 30 === 0) {
+			for (var i = 0; i < width; i+=20) {
+				for (var j = 0; j < height; j+=20){
+					if (villageData.theGrid[(j/20)*(width/20)+(i/20)]) fill(255);
+					else fill(0);
+					rect(i, j, 20, 20);
+				}
+			}
+		}
 
 		for (var i = 0; i < villageData.creatures.people.length; i++) {
 			fill(255);
@@ -46,23 +48,26 @@ function draw() {
 		}
 
 		for (var i = 0; i < villageData.buildings.length; i++) {
-			fill(50, 10);
+			//fill(50);
+			noFill();
+			stroke(0, 255, 0);
 			var bX, bY, bHid, bWit;
 			bX = (villageData.buildings[i].position % (width/20)) * 20;
 			bY = (int)(villageData.buildings[i].position / (width/20)) * 20;
 			bWid = villageData.buildings[i].LnW[0] * 20;
 			bHit = villageData.buildings[i].LnW[1] * 20;
-			rect(bX, bY, bWid, bHit, 5);
+			rect(bX, bY, bWid, bHit, 20);
+			stroke(0);
 		}
 	});
 
-
+count++;
 }
 
 
 
 window.onbeforeunload = function() {
-    return 'Hold up';
+	return 'Hold up';
 }
 
 
@@ -72,7 +77,7 @@ $(window).on('unload', function() {
 		url: "/vilData/"+0,
 		complete: function () {
 			window.onbeforeunload = function() {
-    			return 'Hold up';
+				return 'Hold up';
 			}		
 		}
 	});
--- verver.js
+++ verver.js
@@ -2,11 +2,155 @@
 
 var fs = require("fs");
 var villageData = JSON.parse(fs.readFileSync("./village.json"));
+var falsed = [];
 
+var timeA = 0;
+var timeB = 0;
+var timeDif = 0;
+
+var HValueArr= [];
 
 function randomInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function calcHValue(toSpot, ID) {
+	var temp = 0;
+	for (var i = 0; i < villageData.users[ID].village.theGrid.length; i++) {
+		if (toSpot%50 >= i%50) {
+			temp+=((toSpot%50)-(i%50));
+		} else {
+			temp+=((50-(toSpot%50))-(i%50));
+		}
+		temp+=Math.abs(parseInt(toSpot/50)-parseInt(i/50));
+
+		HValueArr.push(temp);
+		temp = 0;
+	}
+}
+
+calcHValue(1279, 0);
+
+function findIndex(arr, data) {
+    var index;
+ 
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i].data.id === data) {
+            index = i;
+        }
+    }
+ 
+    return index;
+}
+
+function Node(A, B, id) {
+	var C = A + B;
+	this.data = {"id": id, "H": A, "G": B, "F": C};
+	this.parent = null;
+	this.children = [];
+}
+
+function AStarTree(A, B, id) {
+	var node = new Node(A, B, id);
+	this._root = node;
+}
+
+AStarTree.prototype.traverseDF = function(callback) {
+
+    // this is a recurse and immediately-invoking function 
+    (function recurse(currentNode) {
+        // step 2
+        for (var i = 0, length = currentNode.children.length; i < length; i++) {
+            // step 3
+            recurse(currentNode.children[i]);
+        }
+
+        // step 4
+        callback(currentNode);
+
+        // step 1
+    })(this._root);
+
+};
+
+AStarTree.prototype.traverseBF = function(callback) {
+    var queue = [];
+     
+    queue.enqueue(this._root);
+ 
+    currentTree = queue.dequeue();
+ 
+    while(currentTree){
+        for (var i = 0, length = currentTree.children.length; i < length; i++) {
+            queue.enqueue(currentTree.children[i]);
+        }
+ 
+        callback(currentTree);
+        currentTree = queue.dequeue();
+    }
+};
+
+AStarTree.prototype.contains = function(callback, traversal) {
+	traversal.call(this, callback);
+};
+
+AStarTree.prototype.add = function(A, B, id, toData, traversal) {
+	var child = new Node(A, B, id),
+	parent = null,
+	callback = function(node) {
+		if (node.data.id === toData) {
+			parent = node;
+		}
+	};
+
+	this.contains(callback, traversal);
+
+	if (parent) {
+		parent.children.push(child);
+		child.parent = parent;
+	} else {
+		console.log('Cannot add node to a non-existent parent.');
+	}
+};
+
+AStarTree.prototype.remove = function(id, fromData, traversal) {
+    var tree = this,
+        parent = null,
+        childToRemove = null,
+        index;
+ 
+    var callback = function(node) {
+        if (node.data.id === fromData) {
+            parent = node;
+        }
+    };
+ 
+    this.contains(callback, traversal);
+ 
+    if (parent) {
+        index = findIndex(parent.children, id);
+ 
+        if (index === undefined) {
+            console.log('Node to remove does not exist.');
+        } else {
+            childToRemove = parent.children.splice(index, 1);
+        }
+    } else {
+        console.log('Parent does not exist.');
+    }
+ 
+    return childToRemove;
+};
+
+var tree = new AStarTree(HValueArr[1279], 10, 1279);
+tree.add(HValueArr[195], 10, 195, 1279, tree.traverseDF);
+tree.add(HValueArr[86], 10, 86, 1279, tree.traverseDF);
+tree.traverseDF(function(node) {
+	if (node.parent !== null) 
+		console.log("id: "+node.data.id+" H: "+node.data.H+" G: "+node.data.G+" F: "+node.data.F+"--->"+node.parent.data.id);
+	else 
+		console.log("id: "+node.data.id+" H: "+node.data.H+" G: "+node.data.G+" F: "+node.data.F+"--->ROOT");
+});
 
 module.exports = {
 	sendData: function() {
@@ -26,13 +170,20 @@ module.exports = {
 			villageData.users[ID].village.creatures.animals[i].position.y+=randomInt(-1, 1);
 		}
 
+		var date1 = new Date();
+		timeA = date1.getMilliseconds();
 		updateGrid(ID);
+		var date2 = new Date();
+		timeB = date2.getMilliseconds();
+
+		timeDif = timeB - timeA;
+
+		timeDif = 0;
 	},
 	writeToFile: function (ID) {
 		fs.writeFileSync('./village.json', JSON.stringify(villageData, null, 4));
 	}
 }
-
 
 path = function (x, y, stX, stY) {
 
@@ -46,8 +197,8 @@ updateGrid = function (ID) {
 		villageData.users[ID].village.theGrid[villageData.users[ID].village.creatures.animals[i].position] = false;
 	}
 	for (var i = 0; i < villageData.users[ID].village.buildings.length; i++) {
-		for (var j = 0; j < villageData.users[ID].village.buildings[0].LnW[0]-1; j++) {
-			for (var k = 0; k < villageData.users[ID].village.buildings[0].LnW[1]-1; k++) {
+		for (var j = 0; j < villageData.users[ID].village.buildings[i].LnW[0]; j++) {
+			for (var k = 0; k < villageData.users[ID].village.buildings[i].LnW[1]; k++) {
 				villageData.users[ID].village.theGrid[villageData.users[ID].village.buildings[i].position+j+(50*k)] = false;
 			}
 		}
