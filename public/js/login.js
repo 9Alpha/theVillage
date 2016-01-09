a83b66901b2fc548ba61e74bd502e3c9fc7fbab0
teacher-admin <teacher-admin@mbpr1315-01792.fios-router.home>
2016-01-09 18:47:42 -0500

    w/ conor and path

--- public/js/login.js
+++ public/js/login.js
@@ -1,23 +1,44 @@
 
-
-$('#login').on('submit', function(e) {
-	e.preventDefault();
-	var temp = {
-		"info": []
-	};
-	temp.info[0] = ($('#username').val());
-	temp.info[1] = ($('#pass').val());
-	console.log(JSON.stringify(temp));
-	$.ajax({
-		url: '/checkLogin',
-		type: 'POST',
-		data: JSON.stringify(temp),
-		contentType: "application/json",
-		complete: function(data) {
-			console.log(data.responseText);
+$(document).ready(function(){
+	$('#login').on('submit', function(e) {
+		e.preventDefault();
+		var temp = {
+			"info": []
+		};
+		temp.info[0] = ($('#username').val());
+		temp.info[1] = ($('#pass').val());
+		console.log(JSON.stringify(temp));
+		$.ajax({
+			url: '/checkLogin',
+			type: 'POST',
+			data: JSON.stringify(temp),
+			contentType: "application/json",
+			complete: function(data) {
+				console.log(data.responseText);
 				if (data) {
 					console.log("did it");
 				}
 			}
+		});
+		window.location.replace("/options");
 	});
+
+	$(".makeAccount").click(function(){
+		$("#loginPage").empty();
+		$("#loginPage").append("<h2> Create an Account! </h2>");
+		$("#loginPage").append("</br>");
+		$("#loginPage").append("Make a Username!" + " <input type=\"text\" id=\"usernameAcc\">");
+		$("#loginPage").append("</br>");
+		$("#loginPage").append("</br>");
+		$("#loginPage").append("Make a Password!" + " <input type=\"text\" id=\"passwordAcc\">");
+		$("#loginPage").append("</br>");
+		$("#loginPage").append("</br>");
+		$("#loginPage").append("<button class=\"btn btn-primary returntoLogin\"> Submit! </button>");
+
+		$(".returntoLogin").click(function(){
+			$("#loginPage").empty();
+			window.location.replace("/options");
+		});
+	});
+
 });
--- verver.js
+++ verver.js
@@ -12,6 +12,10 @@ var HValueArr= [];
 
 var doIt = true;
 
+var lastNode = 0;
+
+var arrForParents = [];
+
 function randomInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -31,29 +35,29 @@ function calcHValue(toSpot, ID) {
 	}
 }
 
-calcHValue(1329, 0);
+
 
 function findIndex(arr, data) {
-    var index;
- 
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i].data.id === data) {
-            index = i;
-        }
-    }
- 
-    return index;
+	var index;
+
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i].data.id === data) {
+			index = i;
+		}
+	}
+
+	return index;
 }
 
-function Node(A, B, id) {
+function Node(A, B, id, check) {
 	var C = A + B;
-	this.data = {"id": id, "H": A, "G": B, "F": C};
+	this.data = {"id": id, "H": A, "G": B, "F": C, "check": check};
 	this.parent = null;
 	this.children = [];
 }
 
-function AStarTree(A, B, id) {
-	var node = new Node(A, B, id);
+function AStarTree(A, B, id, check) {
+	var node = new Node(A, B, id, check);
 	this._root = node;
 }
 
@@ -76,28 +80,28 @@ AStarTree.prototype.traverseDF = function(callback) {
 };
 
 AStarTree.prototype.traverseBF = function(callback) {
-    var queue = [];
-     
-    queue.enqueue(this._root);
- 
-    currentTree = queue.dequeue();
- 
-    while(currentTree){
-        for (var i = 0, length = currentTree.children.length; i < length; i++) {
-            queue.enqueue(currentTree.children[i]);
-        }
- 
-        callback(currentTree);
-        currentTree = queue.dequeue();
-    }
+	var queue = [];
+
+	queue.enqueue(this._root);
+
+	currentTree = queue.dequeue();
+
+	while(currentTree){
+		for (var i = 0, length = currentTree.children.length; i < length; i++) {
+			queue.enqueue(currentTree.children[i]);
+		}
+
+		callback(currentTree);
+		currentTree = queue.dequeue();
+	}
 };
 
 AStarTree.prototype.contains = function(callback, traversal) {
 	traversal.call(this, callback);
 };
 
-AStarTree.prototype.add = function(A, B, id, toData, traversal) {
-	var child = new Node(A, B, id),
+AStarTree.prototype.add = function(A, B, id, check, toData, traversal) {
+	var child = new Node(A, B, id, check),
 	parent = null,
 	callback = function(node) {
 		if (node.data.id === toData) {
@@ -111,42 +115,43 @@ AStarTree.prototype.add = function(A, B, id, toData, traversal) {
 		parent.children.push(child);
 		child.parent = parent;
 	} else {
-		console.log('Cannot add node to a non-existent parent.');
+		console.log('Cannot add node to a non-existent parent. '+toData);
 	}
 };
 
 AStarTree.prototype.remove = function(id, fromData, traversal) {
-    var tree = this,
-        parent = null,
-        childToRemove = null,
-        index;
- 
-    var callback = function(node) {
-        if (node.data.id === fromData) {
-            parent = node;
-        }
-    };
- 
-    this.contains(callback, traversal);
- 
-    if (parent) {
-        index = findIndex(parent.children, id);
- 
-        if (index === undefined) {
-            console.log('Node to remove does not exist.');
-        } else {
-            childToRemove = parent.children.splice(index, 1);
-        }
-    } else {
-        console.log('Parent does not exist.');
-    }
- 
-    return childToRemove;
+	var tree = this,
+	parent = null,
+	childToRemove = null,
+	index;
+
+	var callback = function(node) {
+		if (node.data.id === fromData) {
+			parent = node;
+		}
+	};
+
+	this.contains(callback, traversal);
+
+	if (parent) {
+		index = findIndex(parent.children, id);
+
+		if (index === undefined) {
+			console.log('Node to remove does not exist.');
+		} else {
+			childToRemove = parent.children.splice(index, 1);
+		}
+	} else {
+		console.log('Parent does not exist.');
+	}
+
+	return childToRemove;
 };
 
-var tree = new AStarTree(HValueArr[1279], 10, 1279);
-tree.add(HValueArr[195], 10, 195, 1279, tree.traverseDF);
-tree.add(HValueArr[86], 10, 86, 1279, tree.traverseDF);
+var tree = new AStarTree(HValueArr[1279], 10, 1279, true);
+tree.add(HValueArr[195], 10, 195, false, 1279, tree.traverseDF);
+tree.add(HValueArr[86], 10, 86, false, 1279, tree.traverseDF);
+tree.remove(195, 1279, tree.traverseDF);
 tree.traverseDF(function(node) {
 	if (node.parent !== null) 
 		console.log("id: "+node.data.id+" H: "+node.data.H+" G: "+node.data.G+" F: "+node.data.F+"--->"+node.parent.data.id);
@@ -183,12 +188,22 @@ module.exports = {
 		timeDif = 0;
 
 
-		var from = 1331;
+		var from = 1337;
+		var to = 1329;
 		if (doIt) {
-			var openList = new AStarTree(0, 0, from);
-			var closedList = new AStarTree(0, 0, -1);
-			closedList.add(0, 0, from, -1, closedList.traverseDF);
-			//path(from, 1329, openList, closedList, ID);
+			var openList = new AStarTree(0, 0, from, 'true');
+			var closedList = new AStarTree(0, 0, 2501, 'true');
+			closedList.add(0, 0, from, 'true', 2501, closedList.traverseDF);
+			calcHValue(to, 0);
+			console.log("path is "+path(from, to, openList, closedList, ID));
+			openList.traverseDF(function(node) {
+				if (node.parent !== null) 
+					console.log("id: "+node.data.id+" H: "+node.data.H+" G: "+node.data.G+" F: "+node.data.F+"--->"+node.parent.data.id);
+				else 
+					console.log("id: "+node.data.id+" H: "+node.data.H+" G: "+node.data.G+" F: "+node.data.F+"--->ROOT");
+			});
+			arrForParents = [];
+			console.log(traceParents(openList, lastNode));
 			doIt = false;
 		}
 
@@ -198,34 +213,63 @@ module.exports = {
 	}
 }
 
+traceParents = function (list, start) {
+	next = true;
+	toLook = start;
+	arrForParents.push(start);
+	while (next) {
+		list.traverseDF(function(node){
+			if (node.data.id === toLook) {
+				if (node.parent !== null) {
+					arrForParents.push(node.parent.data.id);
+					toLook = node.parent.data.id;
+				} 
+				else {
+					next = false;
+				}
+			}
+		});
+	}
+	return arrForParents;
+}
+
 path = function (start, target, open, closed, ID) {
 	lookAround(start, open, closed, ID);
 	var lowest = 50000000000000;
 	var lowestID = -1;
 	var next = true;
 	open.traverseDF(function(node) {
-		if (node.data.F < lowest) {
+		//console.log("id: "+node.data.id+" check: "+node.data.check);
+		if (node.data.F < lowest && node.data.check === 'false') {
 			lowest = node.data.F;
 			lowestID = node.data.id;
 		}
 	});
 
-	//console.log(lowestID);
-
+	if (lowestID === -1) {
+		next = false;
+	}
+	console.log(lowestID);
+	lastNode = lowestID;
 	open.contains(function(node) {
 		if (node.data.id === lowestID) {
 			if (node.data.H === 1) {
 				console.log("finished path");
 				next = false;
 			}
+			else {
+				node.data.check = 'true';
+			}
 		}
 	}, open.traverseDF);
 
 	if (next) {
+		console.log("--------------------");
+		closed.add(0, 0, lowestID, 'true', 2501, closed.traverseDF);
 		path(lowestID, target, open, closed, ID);
 	}
 
-	return true;
+	return lowestID;
 }
 
 lookAround = function (start, open, closed, ID) {
@@ -272,7 +316,8 @@ lookAround = function (start, open, closed, ID) {
 			cs[i] = false;
 		}
 		if (cs[i]) {
-			open.add(HValueArr[spots[i]], move[i]+parentMove, spots[i], start, open.traverseDF);
+			open.add(HValueArr[spots[i]], move[i]+parentMove, spots[i], 'false', start, open.traverseDF);
+			//console.log("added");
 		}
 	}
 
