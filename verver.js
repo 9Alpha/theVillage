a22a2457ff127a97f5674da0ffca6f7f2f5aa6d5
Max Mitchell <mmitchell4488@yahoo.com>
2016-01-18 20:40:36 -0500

    paths actually work

--- verver.js
+++ verver.js
@@ -23,11 +23,7 @@ function randomInt(min, max) {
 function calcHValue(toSpot, ID) {
 	var temp = 0;
 	for (var i = 0; i < villageData.users[ID].village.theGrid.length; i++) {
-		if (toSpot%50 >= i%50) {
-			temp+=((toSpot%50)-(i%50));
-		} else {
-			temp+=((50-(toSpot%50))-(50-(i%50)));
-		}
+		temp+=Math.abs(((toSpot%50)-(i%50)));
 		temp+=Math.abs(parseInt(toSpot/50)-parseInt(i/50));
 
 		HValueArr.push(temp);
@@ -50,14 +46,14 @@ function findIndex(arr, data) {
 }
 
 function Node(A, B, id, dir, check) {
-	var C = A + B;
+	var C = A * B;
 	this.data = {"id": id, "H": A, "G": B, "F": C, "dir":dir, "check": check};
 	this.parent = null;
 	this.children = [];
 }
 
 function AStarTree(A, B, id, dir, check) {
-	var node = new Node(A, B, id, check);
+	var node = new Node(A, B, id, dir, check);
 	this._root = node;
 }
 
@@ -115,7 +111,7 @@ AStarTree.prototype.add = function(A, B, id, dir, check, toData, traversal) {
 		parent.children.push(child);
 		child.parent = parent;
 	} else {
-		console.log('Cannot add node to a non-existent parent. '+toData);
+		console.log('Cannot add node to a non-existent parent.  Parent--> '+toData);
 	}
 };
 
@@ -182,19 +178,16 @@ module.exports = {
 						goodTarget = true;
 					}
 				}
-				var openList = new AStarTree(0, 0, from, 8, 'true');
-				var closedList = new AStarTree(0, 0, 2501, 8, 'true');
-				closedList.add(0, 0, from, 8, 'true', 2501, closedList.traverseDF);
 				HValueArr = [];
 				calcHValue(to, 0);
-				//console.log(ID);
-				console.log("path is "+path(from, to, openList, closedList, ID));
+				var openList = new AStarTree(HValueArr[from], 0, from, 8, 'true');
+				findSuccessors(from, to, openList, ID);
 				arrForParents = [];
-				villageData.users[ID].village.creatures.people[i].pathArr = traceParents(openList, lastNode).reverse();
+				villageData.users[ID].village.creatures.people[i].pathArr = traceParents(openList, to).reverse();
 				villageData.users[ID].village.creatures.people[i].makePath = false;
 			}
 
-			if (count%10 === 0) {
+			if (count%5 === 0) {
 				if (villageData.users[ID].village.creatures.people[i].pathSpot < villageData.users[ID].village.creatures.people[i].pathArr.length) {
 					villageData.users[ID].village.theGrid[villageData.users[ID].village.creatures.people[i].position] = true;
 					villageData.users[ID].village.creatures.people[i].position = villageData.users[ID].village.creatures.people[i].pathArr[villageData.users[ID].village.creatures.people[i].pathSpot];
@@ -226,6 +219,11 @@ module.exports = {
 
 	},
 	writeToFile: function (ID) {
+		for (var i = 0; i < 2500; i++) {
+			if (i % 50 === 0 || i % 50 === 49 || i < 50 || i > 2449) {
+				villageData.users[ID].village.theGrid[i] = false;
+			}
+		}
 		fs.writeFileSync('./village.json', JSON.stringify(villageData, null, 4));
 	}
 }
@@ -252,6 +250,7 @@ traceParents = function (list, start) {
 	toLook = start;
 	arrForParents.push(start);
 	while (next) {
+		//console.log("looking for parents");
 		list.traverseDF(function(node){
 			if (node.data.id === toLook) {
 				if (node.parent !== null) {
@@ -267,55 +266,28 @@ traceParents = function (list, start) {
 	return arrForParents;
 }
 
-path = function (start, target, open, closed, ID) {
-	//console.log(ID+"    in path");
-	var temp = findSuccessors(start, target, open, closed, ID);
+
+findSuccessors = function (start, target, open, ID) {
 	var move = [10, 14, 10, 14, 10, 14, 10, 14];
-	var notOpen = [true, true, true, true, true, true, true, true];
 	var spots = [start-50, start-50+1, start+1, start+50+1, start+50, start+50-1, start-1, start-50-1];
-	var parentMove = 0;
-	
-
-	if (temp === 0) {
-		return true;
-	}
-
-	else if (temp === null) {
-		path(temp.id, target, open, closed, ID);
-	}
-
-	else if (typeof temp === 'object') {
-		closed.add(0, 0, temp.id, 8, 'true', 2501, closed.traverseDF);
-		open.add(HValueArr[temp.id], temp.parentMove, temp.id, temp.dir, 'false', start, open.traverseDF);
-		path(temp.id, target, open, closed, ID);
-	}
-	
-	
-	
-
-}
-
-findSuccessors = function (start, target, open, closed, ID) {
-	//console.log(ID+"   in succ");
-	var move = [10, 14, 10, 14, 10, 14, 10, 14];
 	var parentMove = 0;
 	var temp  = null;
 	var dirNext;
+	var lowest = 50000000000000;
+	var lowestID = -1;
+	var nextID = -1;
+	var quit = false;
 
 	open.contains(function(node) {
 		if (node.data.id === start) {
 			parentMove = node.data.G;
-			parent = node;
 		}
 	}, open.traverseDF);
 
-	var cs = lookAround(start, parentMove, open, closed, ID);
+	var cs = lookAround(start, parentMove, open, ID);
 
-	var lowest = 50000000000000;
-	var lowestID = -1;
-	var next = true;
+
 	open.traverseDF(function(node) {
-		//console.log("id: "+node.data.id+" check: "+node.data.check);
 		if (node.data.F < lowest && node.data.check === 'false') {
 			lowest = node.data.F;
 			lowestID = node.data.id;
@@ -323,183 +295,49 @@ findSuccessors = function (start, target, open, closed, ID) {
 		}
 	});
 
-	if (lowestID === -1) {
-		next = false;
-	}
 
-	console.log(lowestID);
 
-	if (next) {
-		open.contains(function(node) {
-			if (node.data.id === lowestID) {
-				if (node.data.H === 1) {
-					console.log("finished path");
-					return 0;
-				}
-				else {
-					node.data.check = 'true';
-				}
+	open.traverseDF(function(node) {
+		if (node.data.id === lowestID) {
+			if (node.data.H === 0) {
+				quit = true;
 			}
-		}, open.traverseDF);
+			else {
+				node.data.check = 'true';
+			}
+		}
+	});
+
+	if (quit) return true;
 
 
-		//console.log("--------------------");
-		temp = jump(lowestID, target, dirNext, parentMove+move[dirNext], ID);
+	return findSuccessors(lowestID, target, open, ID);
 
-		if (temp)
-		return temp;
-	}
-
-	return findSuccessors(lowestID, target, open, closed, ID);
 }
 
-jump = function (start, target, dir, parentMove, ID) {
-	
-	var move = [10, 14, 10, 14, 10, 14, 10, 14];
-	var spots = [start-50, start-50+1, start+1, start+50+1, start+50, start+50-1, start-1, start-50-1];
-
-	//console.log(ID+"   in jump");
-	if (!villageData.users[ID].village.theGrid[start]) {
-		return null;
-	}
-
-	if (spots[dir] === target) {
-		return {"id": spots[dir], "dir": dir, "cost": parentMove};
-	}
-
-	else if (dir === 1 || dir === 3 || dir === 5 || dir === 7) {
-		if (dir === 1) {
-			if (!villageData.users[ID].village.theGrid[spots[6]]) {
-				if (villageData.users[ID].village.theGrid[spots[0]]) {
-					return {"id": spots[0], "dir": dir, "cost": parentMove};
-				}
+checkIfTrue = function(spot, list) {
+	var good = true;
+	list.traverseDF(function(node) {
+		if (node.data.id === spot) {
+			if (node.data.id === 'true') {
+				good = false;
 			}
-			if (!villageData.users[ID].village.theGrid[spots[4]]) {
-				if (villageData.users[ID].village.theGrid[spots[2]]) {
-					return {"id": spots[2], "dir": dir, "cost": parentMove};
-				}
-			}
-		} else if (dir === 3) {
-			if (!villageData.users[ID].village.theGrid[spots[0]]) {
-				if (villageData.users[ID].village.theGrid[spots[2]]) {
-					return {"id": spots[2], "dir": dir, "cost": parentMove};
-				}
-			}
-			if (!villageData.users[ID].village.theGrid[spots[6]]) {
-				if (villageData.users[ID].village.theGrid[spots[4]]) {
-					return {"id": spots[4], "dir": dir, "cost": parentMove};
-				}
-			}
-		} else if (dir === 5) {
-			if (!villageData.users[ID].village.theGrid[spots[2]]) {
-				if (villageData.users[ID].village.theGrid[spots[4]]) {
-					return {"id": spots[4], "dir": dir, "cost": parentMove};
-				}
-			}
-			if (!villageData.users[ID].village.theGrid[spots[0]]) {
-				if (villageData.users[ID].village.theGrid[spots[6]]) {
-					return {"id": spots[6], "dir": dir, "cost": parentMove};
-				}
-			}
-		} else if (dir === 7) {
-			if (!villageData.users[ID].village.theGrid[spots[4]]) {
-				if (villageData.users[ID].village.theGrid[spots[6]]) {
-					return {"id": spots[6], "dir": dir, "cost": parentMove};
-				}
-			}
-			if (!villageData.users[ID].village.theGrid[spots[2]]) {
-				if (villageData.users[ID].village.theGrid[spots[0]]) {
-					return {"id": spots[0], "dir": dir, "cost": parentMove};
-				}
-			}
+			return false;
 		}
+	});
 
-		if (dir === 1) {
-			if (jump(spots[2], target, 2, parentMove+move[2], ID) != null || jump(spots[0], target, 0, parentMove+move[0], ID) != null) {
-				return {"id": spots[dir], "dir": dir, "cost": parentMove};
-			}
-		}
-		else if (dir === 3) {
-			if (jump(spots[2], target, 2, parentMove+move[2], ID) != null || jump(spots[4], target, 4, parentMove+move[4], ID) != null) {
-				return {"id": spots[dir], "dir": dir, "cost": parentMove};
-			}
-		}
-		else if (dir === 5) {
-			if (jump(spots[6], target, 6, parentMove+move[6], ID) != null || jump(spots[4], target, 4, parentMove+move[4], ID) != null) {
-				return {"id": spots[dir], "dir": dir, "cost": parentMove};
-			}
-		}
-		else if (dir === 7) {
-			if (jump(spots[6], target, 6, parentMove+move[6], ID) != null || jump(spots[0], target, 0, parentMove+move[0], ID) != null) {
-				return {"id": spots[dir], "dir": dir, "cost": parentMove};
-			}
-		}
-	} else {
-    	if (dir === 0) {//up
-    		if (!villageData.users[ID].village.theGrid[spots[6]]) {
-    			if (villageData.users[ID].village.theGrid[spots[7]]) {
-    				return {"id": spots[7], "dir": dir, "cost": parentMove};
-    			}
-    		}
-    		if (!villageData.users[ID].village.theGrid[spots[2]]) {
-    			if (villageData.users[ID].village.theGrid[spots[1]]) {
-    				return {"id": spots[1], "dir": dir, "cost": parentMove};
-    			}
-    		}
-    	} else if (dir === 4) {//down
-    		if (!villageData.users[ID].village.theGrid[spots[6]]) {
-    			if (villageData.users[ID].village.theGrid[spots[5]]) {
-    				return {"id": spots[5], "dir": dir, "cost": parentMove};
-    			}
-    		}
-    		if (!villageData.users[ID].village.theGrid[spots[2]]) {
-    			if (villageData.users[ID].village.theGrid[spots[3]]) {
-    				return {"id": spots[3], "dir": dir, "cost": parentMove};
-    			}
-    		}
-    	} else if (dir === 2) {//right
-    		if (!villageData.users[ID].village.theGrid[spots[0]]) {
-    			if (villageData.users[ID].village.theGrid[spots[1]]) {
-    				return {"id": spots[1], "dir": dir, "cost": parentMove};
-    			}
-    		}
-    		if (!villageData.users[ID].village.theGrid[spots[4]]) {
-    			if (villageData.users[ID].village.theGrid[spots[3]]) {
-    				return {"id": spots[3], "dir": dir, "cost": parentMove};
-    			}
-    		}
-    	} else if (dir === 6) {//left
-    		if (!villageData.users[ID].village.theGrid[spots[0]]) {
-    			if (villageData.users[ID].village.theGrid[spots[7]]) {
-    				return {"id": spots[7], "dir": dir, "cost": parentMove};
-    			}
-    		}
-    		if (!villageData.users[ID].village.theGrid[spots[4]]) {
-    			if (villageData.users[ID].village.theGrid[spots[5]]) {
-    				return {"id": spots[5], "dir": dir, "cost": parentMove};
-    			}
-    		}
-    	}
-    }
-
-    return jump(spots[dir], target, dir, parentMove+move[dir], ID)
+	if (good) return true;
+	return false;
 }
 
 
-lookAround = function (start, parentMove, open, closed, ID) {
+
+lookAround = function (start, parentMove, open, ID) {
 	var cs = [true, true, true, true, true, true, true, true];
 	var move = [10, 14, 10, 14, 10, 14, 10, 14];
-	var notOpen = [true, true, true, true, true, true, true, true];
+	var opens = [false, false, false, false, false, false, false, false];
 	var spots = [start-50, start-50+1, start+1, start+50+1, start+50, start+50-1, start-1, start-50-1];
 	var parent;
-
-	closed.contains(function(node) {
-		for (var i = 0; i < 8; i++) {
-			if (node.data.id === spots[i]) {
-				cs[i] = false;
-			}
-		}
-	}, closed.traverseDF);
 
 	for (var i = 0; i < 8; i++) {
 		if (villageData.users[ID].village.theGrid[spots[i]] === false) {
@@ -515,23 +353,28 @@ lookAround = function (start, parentMove, open, closed, ID) {
 		if (node.data.id === start) {
 			parent = node;
 		}
+	}, open.traverseDF);
+
+	open.traverseDF(function(node) {
 		for (var i = 0; i < 8; i++) {
 			if (node.data.id === spots[i]) {
-				notOpen[i] = false;
+				opens[i] = true;
 			}
 		}
-	}, open.traverseDF);
+	});
 
 	for (var i = 0; i < 8; i++) {
 		if (cs[i]) {
-			if (notOpen[i] === false) {
+			if (opens[i]) {
 				open.contains(function(node) {
 					if (node.data.id === spots[i]) {
 						if (node.data.G > move[i]+parentMove) {
+							node.data.G = move[i]+parentMove;
 							node.parent = parent;
 						}
 					}
 				}, open.traverseDF);
+				cs[i] = false;
 			}
 			else {
 				open.add(HValueArr[spots[i]], move[i]+parentMove, spots[i], i, 'false', start, open.traverseDF);
