56498359ec04f596e2c8f62d23a8c6890768760f
Yuen <conor.yuen16@mbpr1315-01786.trinity.nyc.ny.us>
2016-01-21 11:50:26 -0500

    more options

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,9 @@
 
 $(document).ready(function(){
+	var mainHolder = "";
+	mainHolder += "<h2> Selected Options:  </h2>";
+	$("#optionsPage").append(mainHolder);
+
 	$('#login').on('submit', function(e) {
 		e.preventDefault();
 		var temp = {
@@ -26,7 +30,7 @@ $(document).ready(function(){
 	$(".makeAccount").click(function(){
 		var holder = "";
 		holder += "<form id = \"checkFields\">";
-		holder += "<h2> Create an Account! </h2>"
+		holder += "<h2> Create an Account! </h2>";
 		holder += "</br>";
 		holder += "Make a Username!" + " <input type=\"text\" id=\"usernameAcc\" required>";
 		holder += "</br>";
@@ -38,24 +42,238 @@ $(document).ready(function(){
 		holder += "</form>";
 		$("#loginPage").empty();
 		$("#loginPage").append(holder);
-		/*
-		$("#loginPage").append("<form id = \"checkFields\">");
-		$("#loginPage").append("<h2> Create an Account! </h2>");
-		$("#loginPage").append("</br>");
-		$("#loginPage").append("Make a Username!" + " <input type=\"text\" id=\"usernameAcc\" required>");
-		$("#loginPage").append("</br>");
-		$("#loginPage").append("</br>");
-		$("#loginPage").append("Make a Password!" + " <input type=\"text\" id=\"passwordAcc\" required>");
-		$("#loginPage").append("</br>");
-		$("#loginPage").append("</br>");
-		$("#loginPage").append("<input class=\"btn btn-primary\" type=\"submit\" id=\"createAccount\" value=\"Submit!\">");
-		$("#loginPage").append("</form>");*/
 
 		$("#checkFields").on('submit', function(e) {
 			e.preventDefault();
+			var accInfo = {
+				"details":[]
+			};
+			accInfo.info[0] = ($('#usernameAcc').val());
+			accInfo.info[1] = ($('#passwordAcc').val());
+			console.log(JSON.stringify(accInfo));
+			$.ajax({
+			url: '/makeLogin',
+			type: 'POST',
+			data: JSON.stringify(accInfo),
+			contentType: "application/json",
+			complete: function(data) {
+				console.log(data.responseText);
+				if (data) {
+					console.log("successful account");
+				}
+			}
+		});
 			$("#loginPage").empty();
 			window.location.replace("/options");
 		});
+
 	});
+
+	$(".sunnyClick").click(function(){
+		var holder = "";
+		holder += "Sunny, ";
+		$("#optionsPage").append(holder);
+
+	});
+
+	$(".cloudyClick").click(function(){
+		var holder = "";
+		holder += "Cloudy, ";
+		$("#optionsPage").append(holder);
+
+	});
+
+	$(".rainClick").click(function(){
+		var holder = "";
+		holder += "Rain, ";
+		$("#optionsPage").append(holder);
+
+	});
+
+	$(".snowyClick").click(function(){
+		var holder = "";
+		holder += "Snowy, ";
+		$("#optionsPage").append(holder);
+
+	});
+
+	$(".rockyClick").click(function(){
+		var holder = "";
+		holder += "Rocky, ";
+		$("#optionsPage").append(holder);
+
+	});
+
+	$(".sandyClick").click(function(){
+		var holder = "";
+		holder += "Sandy, ";
+		$("#optionsPage").append(holder);
+
+	});
+
+	$(".treesClick").click(function(){
+		var holder = "";
+		holder += "Trees, ";
+		$("#optionsPage").append(holder);
+
+	});
+
+	$(".mountainsClick").click(function(){
+		var holder = "";
+		holder += "Mountains, ";
+		$("#optionsPage").append(holder);
+
+	});
+
+	$(".onewolfClick").click(function(){
+		var holder = "";
+		holder += "One Wolf, ";
+		$("#optionsPage").append(holder);
+
+	});
+
+	$(".twowolvesClick").click(function(){
+		var holder = "";
+		holder += "Two Wolves, ";
+		$("#optionsPage").append(holder);
+
+	});
+
+	$(".threewolvesClick").click(function(){
+		var holder = "";
+		holder += "Three Wolves, ";
+		$("#optionsPage").append(holder);
+
+	});
+
+	$(".fourwolvesClick").click(function(){
+		var holder = "";
+		holder += "Four Wolves, ";
+		$("#optionsPage").append(holder);
+
+	});
+
+	$(".fivewolvesClick").click(function(){
+		var holder = "";
+		holder += "Five Wolves, ";
+		$("#optionsPage").append(holder);
+
+	});
+
+	$(".onecatClick").click(function(){
+		var holder = "";
+		holder += "One Cat, ";
+		$("#optionsPage").append(holder);
+
+	});
+
+	$(".twocatsClick").click(function(){
+		var holder = "";
+		holder += "Two Cats, ";
+		$("#optionsPage").append(holder);
+
+	});
+
+	$(".threecatsClick").click(function(){
+		var holder = "";
+		holder += "Three Cats, ";
+		$("#optionsPage").append(holder);
+
+	});
+
+	$(".fourcatsClick").click(function(){
+		var holder = "";
+		holder += "Four Cats, ";
+		$("#optionsPage").append(holder);
+
+	});
+
+	$(".fivecatsClick").click(function(){
+		var holder = "";
+		holder += "Five Cats, ";
+		$("#optionsPage").append(holder);
+
+	});
+
+	$(".onechildClick").click(function(){
+		var holder = "";
+		holder += "One Child, ";
+		$("#optionsPage").append(holder);
+
+	});
+
+	$(".twochildrenClick").click(function(){
+		var holder = "";
+		holder += "Two Children, ";
+		$("#optionsPage").append(holder);
+
+	});
+
+
+	$(".threechildrenClick").click(function(){
+		var holder = "";
+		holder += "Three Children, ";
+		$("#optionsPage").append(holder);
+
+	});
+
+
+	$(".fourchildrenClick").click(function(){
+		var holder = "";
+		holder += "Four Children, ";
+		$("#optionsPage").append(holder);
+
+	});
+
+	$(".fivechildrenClick").click(function(){
+		var holder = "";
+		holder += "Five Children, ";
+		$("#optionsPage").append(holder);
+
+	});
+
+	$(".fivechildrenClick").click(function(){
+		var holder = "";
+		holder += "Five Children, ";
+		$("#optionsPage").append(holder);
+
+	});
+
+	$(".oneworkerClick").click(function(){
+		var holder = "";
+		holder += "One Worker ";
+		$("#optionsPage").append(holder);
+
+	});
+
+	$(".twoworkersClick").click(function(){
+		var holder = "";
+		holder += "Two Workers ";
+		$("#optionsPage").append(holder);
+
+	});
+
+	$(".threeworkersClick").click(function(){
+		var holder = "";
+		holder += "Three Workers ";
+		$("#optionsPage").append(holder);
+
+	});
+
+	$(".fourworkersClick").click(function(){
+		var holder = "";
+		holder += "Four Workers ";
+		$("#optionsPage").append(holder);
+
+	});
+
+
+	$(".fiveworkersClick").click(function(){
+		var holder = "";
+		holder += "Five Workers ";
+		$("#optionsPage").append(holder);
+
+	});
+
 
 });
